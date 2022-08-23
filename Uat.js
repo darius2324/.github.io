46bcd51d648e8a5c737ab639a3c977e83158a02d
darius2324 <109635068+darius2324@users.noreply.github.com>
2022-08-23 09:15:00 -0500

    Add files via upload

---
+++ Uat.js
@@ -0,0 +1,10 @@
+
+    function start(){
+     document.getElementById("startbtn").disabled=true;
+     document.getElementById("stopbtn").disabled=false; 
+     //start and stop disable
+    }
+    function stop(){
+document.getElementById("startbtn").disabled=false;
+document.getElementById("stopbtn").disabled=true;
+}
